08bb524a8ec81f083c6c28880c0191328af69a8c
dimonstr223 <dimstukmikolu@gmail.com>
2023-01-19 08:55:29 +0300

    fetchPosts extraReducer created

--- src/redux/slices/posts.js
+++ src/redux/slices/posts.js
@@ -21,6 +21,20 @@ const postsSlice = createSlice({
 	name: 'posts',
 	initialState,
 	ruducer: {},
+	extraReducers: {
+		[fetchPosts.pending]: state => {
+			state.posts.items = []
+			state.posts.status = 'loading'
+		},
+		[fetchPosts.fulfilled]: (state, action) => {
+			state.posts.items = action.payload
+			state.posts.status = 'loaded'
+		},
+		[fetchPosts.rejected]: state => {
+			state.posts.items = []
+			state.posts.status = 'error'
+		},
+	},
 })
 
 export const postsReducer = postsSlice.reducer
