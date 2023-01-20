d51f561918e539c4dffca28d1284f65896ea32c6
dimonstr223 <dimstukmikolu@gmail.com>
2023-01-20 11:40:39 +0300

    removing token when log out clicked

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -12,7 +12,10 @@ export const Header = () => {
 	const isAuth = useSelector(selectIsAuth)
 
 	const onClickLogout = () => {
-		if (window.confirm('Are you sure you want to log out?')) dispatch(logout())
+		if (window.confirm('Are you sure you want to log out?')) {
+			dispatch(logout())
+			window.localStorage.removeItem('token')
+		}
 	}
 
 	return (
