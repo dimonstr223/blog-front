8589f2457fe689b0a3e43c01db49cdb9e0c76ae6
dimonstr223 <dimstukmikolu@gmail.com>
2023-01-21 06:50:25 +0300

    fetchRegister thunk created

--- src/redux/slices/auth.js
+++ src/redux/slices/auth.js
@@ -11,6 +11,14 @@ export const fetchAuthMe = createAsyncThunk('auth/fetchAuthMe', async () => {
 	return data
 })
 
+export const fetchRegister = createAsyncThunk(
+	'auth/fetchRegister',
+	async params => {
+		const { data } = await axios.post('auth/register', params)
+		return data
+	}
+)
+
 const initialState = {
 	data: null,
 	status: 'loading',
@@ -37,6 +45,7 @@ const authSlice = createSlice({
 			state.status = 'error'
 			state.data = null
 		},
+
 		[fetchAuthMe.pending]: state => {
 			state.status = 'loading'
 			state.data = null
@@ -46,6 +55,19 @@ const authSlice = createSlice({
 			state.data = action.payload
 		},
 		[fetchAuthMe.rejected]: state => {
+			state.status = 'error'
+			state.data = null
+		},
+
+		[fetchRegister.pending]: state => {
+			state.status = 'loading'
+			state.data = null
+		},
+		[fetchRegister.fulfilled]: (state, action) => {
+			state.status = 'loaded'
+			state.data = action.payload
+		},
+		[fetchRegister.rejected]: state => {
 			state.status = 'error'
 			state.data = null
 		},
