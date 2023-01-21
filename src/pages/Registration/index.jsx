8a37d0c6be616eccfdfca16244428050ac2eb9a9
dimonstr223 <dimstukmikolu@gmail.com>
2023-01-21 06:43:20 +0300

    registration form created

--- src/pages/Registration/index.jsx
+++ src/pages/Registration/index.jsx
@@ -1,27 +1,80 @@
-import React from 'react';
-import Typography from '@mui/material/Typography';
-import TextField from '@mui/material/TextField';
-import Paper from '@mui/material/Paper';
-import Button from '@mui/material/Button';
-import Avatar from '@mui/material/Avatar';
+import React from 'react'
+import Typography from '@mui/material/Typography'
+import TextField from '@mui/material/TextField'
+import Paper from '@mui/material/Paper'
+import Button from '@mui/material/Button'
+import Avatar from '@mui/material/Avatar'
 
-import styles from './Login.module.scss';
+import styles from './Login.module.scss'
+import { useForm } from 'react-hook-form'
 
 export const Registration = () => {
-  return (
-    <Paper classes={{ root: styles.root }}>
-      <Typography classes={{ root: styles.title }} variant="h5">
-        Создание аккаунта
-      </Typography>
-      <div className={styles.avatar}>
-        <Avatar sx={{ width: 100, height: 100 }} />
-      </div>
-      <TextField className={styles.field} label="Полное имя" fullWidth />
-      <TextField className={styles.field} label="E-Mail" fullWidth />
-      <TextField className={styles.field} label="Пароль" fullWidth />
-      <Button size="large" variant="contained" fullWidth>
-        Зарегистрироваться
-      </Button>
-    </Paper>
-  );
-};
+	const {
+		register,
+		handleSubmit,
+		setError,
+		formState: { errors, isValid },
+	} = useForm({
+		defaultValues: {
+			fullName: 'Slim Shady',
+			email: 'slim@shady.com',
+			password: '1234',
+		},
+		mode: 'onChange',
+	})
+
+	const onSubmit = async values => {
+		// const data = await dispatch(fetchAuth(values))
+		// if (!data.payload) {
+		// 	return alert('Authorization error')
+		// }
+		// if ('token' in data.payload) {
+		// 	window.localStorage.setItem('token', data.payload.token)
+		// }
+	}
+
+	React.useEffect(() => {}, [])
+
+	// if (isAuth) {
+	// 	return <Navigate to='/' />
+	// }
+	return (
+		<Paper classes={{ root: styles.root }}>
+			<Typography classes={{ root: styles.title }} variant='h5'>
+				Создание аккаунта
+			</Typography>
+			<div className={styles.avatar}>
+				<Avatar sx={{ width: 100, height: 100 }} />
+			</div>
+			<form onSubmit={handleSubmit(onSubmit)}>
+				<TextField
+					className={styles.field}
+					label='Full name'
+					error={Boolean(errors.fullName?.message)}
+					helperText={errors.fullName?.message}
+					{...register('fullName', { required: 'Enter fullname' })}
+					fullWidth
+				/>
+				<TextField
+					className={styles.field}
+					label='E-Mail'
+					error={Boolean(errors.email?.message)}
+					helperText={errors.email?.message}
+					{...register('email', { required: 'Enter email' })}
+					fullWidth
+				/>
+				<TextField
+					className={styles.field}
+					label='Password'
+					error={Boolean(errors.password?.message)}
+					helperText={errors.password?.message}
+					{...register('password', { required: 'Enter password' })}
+					fullWidth
+				/>
+				<Button type='submit' size='large' variant='contained' fullWidth>
+					Зарегистрироваться
+				</Button>
+			</form>
+		</Paper>
+	)
+}
